9feb54c9707c9b0713ce3406db3d13415f9ceaf7
san38700 <140396812+san38700@users.noreply.github.com>
2023-07-26 23:34:18 +0530

    Update dom2.js

--- dom2.js
+++ dom2.js
@@ -2,4 +2,4 @@ var items = document.getElementsByClassName("list-group-item");
 for (var i = 0; i < items.length; i++) {
   items[i].style.fontWeight = "bold";
 }
-items[2].style.color = "#2AAA8A";
+items[2].style.backgroundColor = "#2AAA8A";
