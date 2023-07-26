88be5d8963480fd1389f440d6a1b957c4c9b33f7
san38700 <140396812+san38700@users.noreply.github.com>
2023-07-26 16:26:55 +0530

    Second Commit

---
+++ dom.js
@@ -0,0 +1,11 @@
+
+
+console.log(document.getElementById("header-title"));
+var headerTitle = document.getElementById("header-title")
+console.log(headerTitle);
+headerTitle.innerHTML = "<h3> Hello </h3>"
+
+var header = document.getElementById("main-header");
+header.style.borderBottom = "solid 3px #000";
+header.style.fontStyle = "italic";
+headerTitle.style.color = "#00FF00";
