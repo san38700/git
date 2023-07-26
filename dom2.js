0c65d341a4e53674dd0c754497c385f06036669f
san38700 <140396812+san38700@users.noreply.github.com>
2023-07-26 23:52:15 +0530

    Update dom2.js

--- dom2.js
+++ dom2.js
@@ -3,3 +3,9 @@ for (var i = 0; i < items.length; i++) {
   items[i].style.fontWeight = "bold";
 }
 items[2].style.backgroundColor = "#2AAA8A";
+
+var li = document.getElementsByTagName("li");
+for (var i = 0; i < li.length; i++) {
+  li[i].style.fontWeight = "bold";
+}
+li[2].style.backgroundColor = "#2AAA8A";
